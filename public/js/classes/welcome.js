6dc5dd689fa71953f711830001f7635944f5065a
Nico G <nicog.trm@gmail.com>
2016-04-17 00:08:40 +0200

    logged player in on enter

--- public/js/classes/welcome.js
+++ public/js/classes/welcome.js
@@ -3,19 +3,18 @@
  * Class responsible for the welcome screen
  */
 var Welcome = function() {
+    var $button = $("#play-button"),
+        $input = $("#username"),
+        main,
+        game,
+        sockets;
     return {
-        init: function(main, game, sockets) {
-            $("#play-button").on('click', function() {
-                var username = $("#username").val();
-                if (username == "") {
-                    if (game.debug()) username = makeid();
-                    else return;
-                }
-                $(this).attr('disabled');
-                $(this).addClass('disabled', 'disabled');
-                main.setUsername(username);
-                sockets.login();
-            });
+        init: function(mainRef, gameRef, socketsRef) {
+            main = mainRef;
+            game = gameRef;
+            sockets = socketsRef;
+            $button.on('click', _logPlayerIn);
+            _loginOnEnter();
         },
         selector: "welcome-screen"
     };
@@ -25,5 +24,23 @@ var Welcome = function() {
         var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
         for (var i = 0; i < 5; i++) text += possible.charAt(Math.floor(Math.random() * possible.length));
         return text;
+    }
+
+    function _logPlayerIn() {
+        var username = $input.val();
+        if (username == "") {
+            if (game.debug()) username = makeid();
+            else return;
+        }
+        $button.attr('disabled');
+        $button.addClass('disabled', 'disabled');
+        main.setUsername(username);
+        sockets.login();
+    }
+
+    function _loginOnEnter() {
+        $input.on("keyup", function(event) {
+            if (event.which == 13 && $(this).val() !== '') _logPlayerIn();
+        });
     }
 };
