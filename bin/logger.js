6c53242cfcc74c923f84ff2b6528c1cfae008ec6
Nico G <nicog.trm@gmail.com>
2016-05-23 00:55:25 +0200

    documented logger.js

--- bin/logger.js
+++ bin/logger.js
@@ -9,23 +9,45 @@ var Logger = function() {
         logFile = _createFileName();
     return {
         writeInitialWords: writeInitialWords,
-        writeMove: writeMove
+        writeMove: _writeGameLine
     };
 
+    /**
+     * Creates a filename for a new log
+     * @returns {string}
+     * @private
+     */
     function _createFileName() {
         return 'gameLogs/' + _getDateTime() + '.csv';
     }
 
+    /**
+     * Returns the current datetime as yyyy-mm-dd hh:mm:ss
+     * @returns {string}
+     * @private
+     */
     function _getDateTime() {
         var now = new Date();
         return now.getFullYear() + '-' + _zeroPad(now.getMonth() + 1) + '-' + _zeroPad(now.getDate()) + ' ' +
             _zeroPad(now.getHours()) + ':' + _zeroPad(now.getMinutes()) + ':' + _zeroPad(now.getSeconds());
 
+        /**
+         * Adds a leading zero to a number if it has only one digit
+         * @param {number} integer The number to use
+         * @returns {string}
+         * @private
+         */
         function _zeroPad(integer) {
             return (integer < 10 ? '0' : '') + integer.toString();
         }
     }
 
+    /**
+     * Logs the game word plus each player's initial word
+     * @param {number|string} round The current round
+     * @param {string} word The game word
+     * @param {Array} playersWords The initial words of all players
+     */
     function writeInitialWords(round, word, playersWords) {
         _writeColumnNames(true);
         _writeGameLine(0, round, word);
@@ -33,10 +55,11 @@ var Logger = function() {
         _writeColumnNames(false);
     }
 
-    function writeMove(userID, round, word) {
-        _writeGameLine(userID, round, word);
-    }
-
+    /**
+     * Writes the column headers
+     * @param {boolean} initial Whether the headers correspond to the initial configurations or to moves made by players
+     * @private
+     */
     function _writeColumnNames(initial) {
         fs.appendFileSync(logFile,
             ['Game ' + (initial ? 'initial code' : 'play') + ' line#',
@@ -45,6 +68,13 @@ var Logger = function() {
         line = 0;
     }
 
+    /**
+     * Logs a row
+     * @param {number|string} userID The player's ID
+     * @param {number|string} round The current round
+     * @param {Array} word The word to be logged
+     * @private
+     */
     function _writeGameLine(userID, round, word) {
         fs.appendFileSync(logFile, ([++line, userID, round, _getDateTime()].concat(word)).join(',') + '\n');
     }
